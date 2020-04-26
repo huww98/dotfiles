64d6fb3f673d15bb65e9b342c76461a2fe4bfa18
胡玮文 <huww98@outlook.com>
2020-04-26 13:13:29 +0800

    add tmux.conf

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 
 BASE_PATH="$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )"
 
-ln --symbolic --force --relative "$BASE_PATH/.vimrc" "$BASE_PATH/.pylintrc" "$HOME"
+ln --symbolic --force --relative \
+    "$BASE_PATH/.vimrc" \
+    "$BASE_PATH/.pylintrc" \
+    "$BASE_PATH/.tmux.conf" \
+    "$HOME"
